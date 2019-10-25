9d186d0126f510fe772c54a5a6fc267584383c2f
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-10-25 14:08:44 +0300

    Update main.c

--- maxmintypes/main.c
+++ maxmintypes/main.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 #include <limits.h>
-
+#include <stdlib.h>
 int main()
 {
-    int num, temp, i;
+    int i;
+    long long num, temp;
     temp=1;
     num=1;
     for(i=0; num>0 ; num<<=1, i++);
-    //printf("%d", i);
     for(num = 0; i>0; temp<<=1, i--)
     {
         num |= temp;
-        //printf("%d %d\n",num,i);
     }
-    printf("%d\n%d\n", INT_MAX, INT_MIN);
-    printf("%d\n", num);
+    printf("%lld\n", num);
     return 0;
 }
