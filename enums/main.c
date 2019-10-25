383e435241ee2db4dd3994a835f53c4c035d73b7
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-10-25 17:16:09 +0300

    Update main.c

--- enums/main.c
+++ enums/main.c
@@ -7,6 +7,9 @@ int main()
     int a,b;
     a=10;
     b=11;
+    char str[]="123";//*{'1','2','3','\0'}*;
+
     printf("%d", a<b?TRUE:FALSE);
+    printf("\nstrlen: %d", strlen(str));
     return 0;
 }
