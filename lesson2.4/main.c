86f55ec36c79ec36f197d4db5f9d5e87bef8eb2b
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-10-29 17:28:13 +0300

    Update main.c

--- lesson2.4/main.c
+++ lesson2.4/main.c
@@ -6,16 +6,19 @@ int main()
     int i,j,k;
     int u;
     char string[] = {'H', 'e', 'l', 'l', 'o','\0'};
-    char string2[] = {'h','e','o','\0'};
-    for(i=0;string2[i]!='\0'; i++)
+    char exmpl[] = {'h','e','o','\0'};
+    char exmplU[sizeof(exmpl)];
+    for(i=0; exmpl[i]!='\0'; i++){
+        if(isupper(exmpl[i]))exmplU[i]=tolower(exmpl[i]);
+        else if(islower(exmpl[i]))exmplU[i]=toupper(exmpl[i]);
+    }
+        printf()
+    for(i=0;exmpl[i]!='\0'; i++)
     {
-        for(k=j=0; string[j] != '\0'; j++)
-            u = isupper(string[j]);
-            printf("%d\n", u);
-            //printf("%d: %c\n",u , string[j]);
-            if(string[j]!=string2[i]);
-            string[k++] = string[j];
-        string[k]='\0';
+        for(j=k=0; string[j]!='\0'; j++)
+        if(string[j]!=exmpl[i])
+        string[k++] = string[j];
+        string[k] = '\0';
     }
     printf("%s\n", string);
     return 0;
