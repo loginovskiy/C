a630559637741d1d8e8bd06afa7ecd112a13d945
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-11-18 02:03:54 +0300

    Create main.c

---
+++ lesson4.2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+double atoff(char num[]);
+int main()
+{
+    double num, atoff(char num[]);
+    char val[] = "22.2";
+    num = atoff(val);
+    printf("%f", num);
+    return 0;
+}
