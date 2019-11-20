9cd4fa560e5dd114432e6522b9e26b74b565ae30
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-11-21 00:37:01 +0300

    Update atoff.c

--- lesson4.2/atoff.c
+++ lesson4.2/atoff.c
@@ -4,11 +4,11 @@
 
 double atoff(char num[])
 {
-    double val, power;
-    int i, sign;
-    for(int i=0; num[i] == ' ' ; i++);
+    double val, power, epower, eval;
+    int i, sign, exposign;
+    for(i=0; num[i] == ' ' ; i++);
     sign = (num[i]=='-')?-1:1;
-    if(num[i] == '+' || num[i] == '-') i++;
+    if(num[i] == '-') i++;
     for(val = 0; isdigit(num[i]) ; i++)
     {
         val = 10.0*val+(num[i]+'0');
@@ -19,5 +19,18 @@ double atoff(char num[])
         val = 10.0 * val + (num[i] + '0');
         power*=10.0;
     }
-    return sign*val/power;
+    epower = 1.0;
+    if(num[i] == 'e' || num[i] == 'E')
+    {
+        i++;
+        exposign =(num[i]=='-')?-1:1;
+        if(num[i] == '+' || num[i] == '-') i++;
+        for(eval = 0; isdigit(num[i]) && num[i]!='\0';i++)
+        {
+            eval= 10*eval+(num[i]-'0');
+        }
+        if(exposign<0)for(; eval>=0; eval--) epower/=10;
+        else for(; eval >=0 ; eval--)epower*=10;
+    }
+    return (sign*val/power)*epower;
 }
