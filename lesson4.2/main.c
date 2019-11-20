337b81822e17589fd8d5650508ced10e1a543769
loginovskiy <38593189+loginovskiy@users.noreply.github.com>
2019-11-21 00:37:08 +0300

    Update main.c

--- lesson4.2/main.c
+++ lesson4.2/main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 double atoff(char num[]);
 int main()
 {
     double num, atoff(char num[]);
-    char val[] = "22.2";
+    char val[] = "22.2e-2";
     num = atoff(val);
     printf("%f", num);
     return 0;
